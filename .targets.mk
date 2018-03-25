50a62bbcf899816bd1db93cdb9a2f8a226404d08
Christopher Wood <christopherwood07@gmail.com>
2018-03-25 13:23:07 -0700

    Updates

--- .targets.mk
+++
@@ -1,2 +0,0 @@
-draft-wood-tls-ticketrequest-00.md: draft-wood-tls-ticketrequest.md
-	sed -e s/draft-wood-tls-shortlivedshare-latest/draft-wood-tls-shortlivedshare-00/g -e s/draft-wood-tls-ticketrequest-latest/draft-wood-tls-ticketrequest-00/g $< >$@
