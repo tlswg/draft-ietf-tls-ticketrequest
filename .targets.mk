27d09c33cffb441381762d1ceeb5e7ca980ff53f
Christopher Wood <christopherwood07@gmail.com>
2018-10-09 22:10:15 -0700

    Updates

--- .targets.mk
+++
@@ -1,4 +0,0 @@
-TARGETS_DRAFTS := draft-wood-tls-ticketrequest
-TARGETS_TAGS := 
-draft-wood-tls-ticketrequest-00.txt: draft-wood-tls-ticketrequest.txt
-	sed -e s/draft-wood-tls-ticketrequest-latest/draft-wood-tls-ticketrequest-00/g -e s/draft-wood-tls-ticketrequest-latest/draft-wood-tls-ticketrequest-00/g -e s/draft-wood-tls-ticketrequest-latest/draft-wood-tls-ticketrequest-00/g $< >$@
